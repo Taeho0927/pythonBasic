6cb1ba91911707fa107bb2b029c8764d94f09779
Taeho0927 <105978681+Taeho0927@users.noreply.github.com>
2022-05-28 20:27:27 +0900

    Delete day6_2.py

--- day6_2.py
+++
@@ -1,9 +0,0 @@
-#함수 연습 2(입력값이 없는 함수)
-#1)hello()라고 함수를 실행하면 "hello python!"이라고 출력되는 함수를 구현하라
-def hello():
-    print('"hello python!"')
-hello()
-#2)print(hello())라고 실행하면 "hello python!"이라고 출력되는 함수를 구현하라
-def hello():
-    return '"hello python!"'
-print(hello())
